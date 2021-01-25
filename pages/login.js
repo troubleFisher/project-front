ca543bd090657408000e449bcb42b07dd08da7bc
tangyu <tangyu@medlinker.com>
2021-01-25 11:05:54 +0800

    feat(login.js): 新增图片验证码

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Form, Input, Button, Checkbox, Divider } from "antd";
-import 'antd/dist/antd.css'
+import "antd/dist/antd.css";
 const layout = {
   labelCol: { span: 8 },
   wrapperCol: { span: 16 },
@@ -11,6 +11,7 @@ const tailLayout = {
 };
 
 const login = () => {
+  const [captchaSrc, setCaptchaSrc] = useState("/api/captcha");
   const onFinish = (values) => {
     console.log("Success:", values);
   };
@@ -19,40 +20,76 @@ const login = () => {
     console.log("Failed:", errorInfo);
   };
 
+  const changeCaptcha = () => {
+    const src = "/api/captcha?_t=" + new Date().getTime();
+    setCaptchaSrc(src);
+  };
+
   return (
-    <Form
-      {...layout}
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: "Please input your username!" }]}
+    <div style={{ width: "80%" }}>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="用户名"
+          name="username"
+          rules={[{ required: true, message: "请填写用户名!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: "Please input your password!" }]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="邮箱"
+          name="email"
+          rules={[{ required: true, message: "请填写邮箱!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+        <Form.Item label="验证码">
+          <Form.Item
+            name="captcha"
+            noStyle
+            rules={[{ required: true, message: "请填写验证码!" }]}
+          >
+            <Input style={{ width: "70%" }} placeholder="请填写验证码" />
+          </Form.Item>
+          <span style={{ margin: "0 8px" }}>
+            <img src={captchaSrc} onClick={changeCaptcha} />
+          </span>
+        </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[{ required: true, message: "请填写密码!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="确认密码"
+          name="password1"
+          rules={[{ required: true, message: "请填写确认密码!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
